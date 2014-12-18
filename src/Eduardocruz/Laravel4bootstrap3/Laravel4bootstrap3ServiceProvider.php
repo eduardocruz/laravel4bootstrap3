48cdd6da6cffd0202ed6fa33f1633ea915e7af50
Eduardo Cruz <eduardo@eduardocruz.com>
2014-12-18 12:27:20 -0300

    initial commit

---
+++ src/Eduardocruz/Laravel4bootstrap3/Laravel4bootstrap3ServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Eduardocruz\Laravel4bootstrap3;
+
+use Illuminate\Support\ServiceProvider;
+
+class Laravel4bootstrap3ServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('eduardocruz/laravel4bootstrap3');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
