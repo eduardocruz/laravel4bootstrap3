a120e44a543b0dc0dfed83fcc5a8647dd767b48d
Eduardo Cruz <eduardo@eduardocruz.com>
2014-12-18 14:06:06 -0300

    Adding home file

---
+++ src/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('laravel4bootstrap3::layouts/master')
+@section('meta-title')
+    {{Config::get('laravel4bootstrap3::app.app.name')}}
+@stop
+@section('container')
+    <h1 class="text-center">Home</h1>
+@stop
+@section('scripts')
+@stop
