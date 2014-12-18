6d2429c9a01dfd6ee640ca458af663ba9ca0f18c
Eduardo Cruz <eduardo@eduardocruz.com>
2014-12-18 14:06:26 -0300

    adding default navbar

---
+++ src/views/layouts/partials/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
+            <span class="sr-only">Toggle navigation</span>
+        </button>
+        <a class="navbar-brand" href="/">
+            @yield('home-title', 'Home' )
+        </a>
+    </div>
+</nav><!-- /navbar -->
+<p>&nbsp;</p>
